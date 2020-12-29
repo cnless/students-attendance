6715eef6b27b296100e3e594b22b17c9ec643cd0
cnless <2514169858@qq.com>
2020-12-29 14:29:57 +0800

    first commit

---
+++ src/main/java/com/dw/StudentsAttendanceApplication.java
@@ -0,0 +1,13 @@
+package com.dw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentsAttendanceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudentsAttendanceApplication.class, args);
+    }
+
+}
